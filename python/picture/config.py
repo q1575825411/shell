ac237e26d620bf02f385792dcd319f59043a1c13
CaoRuis <you.tian@sunmi.com>
2024-04-09 15:16:20 +0800

    update .gitignore

--- python/picture/config.py
+++ python/picture/config.py
@@ -4,7 +4,7 @@
 API_URL = "https://moe.jitsu.top/img/"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 SAVE_DIR_BASE = "pics" 
-COPY_DIR_BASE = "/home/zly/disk/work/write/UPDATE/pics"
+COPY_DIR_BASE = "/home/zly/disk/work/write/UPDATE/pics/pc"
 
 # 参数映射表
 sort_options = {
